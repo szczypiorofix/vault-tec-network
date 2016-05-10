24efb0fca7305492ded311cd033fd6b2b7231d48
szczypiorofix <szczypiorofix@o2.pl>
2016-05-10 10:55:42 +0200

    W trakcie tworzenia podstawowegu GUI

--- src/vaulttecnetwork/VaultTecNetworkServer.java
+++ src/vaulttecnetwork/VaultTecNetworkServer.java
@@ -1,10 +1,33 @@
 package vaulttecnetwork;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class VaultTecNetworkServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private JFrame frame;
+	
+public VaultTecNetworkServer()
+{
+	frame = new JFrame("Vault-Tec Network Server. RobCo Industries.");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(400, 500);
+	frame.setLayout(new BorderLayout());
+	frame.setLocationRelativeTo(null);
+	frame.setVisible(true);	
+}
+	
+public static void main(String[] args) {
 
+EventQueue.invokeLater(new Runnable()
+{
+	public void run()
+	{
+		new VaultTecNetworkServer();
 	}
+});
+}
 
 }
