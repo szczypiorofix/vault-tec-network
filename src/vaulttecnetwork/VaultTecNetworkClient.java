12ef9862595fb61abdaa35e249ac4ffdc1653374
szczypiorofix <szczypiorofix@o2.pl>
2016-05-10 10:04:59 +0200

    Początek projektu Vault-Tec Network

---
+++ src/vaulttecnetwork/VaultTecNetworkClient.java
@@ -0,0 +1,10 @@
+package vaulttecnetwork;
+
+public class VaultTecNetworkClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/vaulttecnetwork/VaultTecNetworkServer.java
@@ -0,0 +1,10 @@
+package vaulttecnetwork;
+
+public class VaultTecNetworkServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
